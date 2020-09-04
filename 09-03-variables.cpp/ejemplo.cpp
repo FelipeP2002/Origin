efabb74a7e87d9d14a077a791d6ea6ca6a0667aa
Dperalta <Dperalta@unal.edu.co>
2020-09-03 22:49:14 -0500

    clase de variables

---
+++ 09-03-variables.cpp/ejemplo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main (int argc , char **argv)
+{
+  std::cout<<argc<<std::endl;
+  std::cout<<argv[0]<<std::endl;
+  std::cout<<argv[1]<<std::endl;
+  return 0;
+}
+
+ 
---
+++ 09-03-variables.cpp/lectura_entrada_standar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc , char **argv)
+
+{
+  std::string fullname;
+  std::cout <<"Hola, escirbe tu nombre porfavor\n";
+  std::getline(std::cin,fullname);
+  std::cout <<"Hola, " <<fullname <<std::endl;
+  
+  return 0;
+}
